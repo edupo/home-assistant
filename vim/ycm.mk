f92a85eba87f3be90d3d5ec01be8b85694c64a17
Eduardo Lezcano <contact@eduardolezcano.com>
2016-11-11 14:37:53 +0100

    ycm depends on cmake for installation

--- vim/ycm.mk
+++ vim/ycm.mk
@@ -6,9 +6,13 @@
 
 PLUGINS     += Valloric/YouCompleteMe
 SECTIONS    += YCM_SECTION
+PACKAGES    += cmake ycm
 
 define YCM_SECTION
 " YCM
 let g:ycm_autoclose_preview_window_after_completion=1
 map <leader>g  :YcmCompleter GoToDefinitionElseDeclaration<CR>
 endef
+
+ycm.install:
+	$(_VIM_BUNDLE_DIR)/YouCompleteMe/install.py --clang-completer
