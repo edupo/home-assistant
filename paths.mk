d5e516516a83c59b2b15be58993d670b4b0e931e
Eduardo Lezcano <eduardo.lezcano@be.atlascopco.com>
2016-11-18 07:35:09 +0100

    Added paths file to centralize those

--- paths.mk
+++ paths.mk
@@ -1,4 +1,5 @@
 #!/usr/bin/make
 # System paths definition.
 
-export SYSTEMD_SYSTEM_DIR=/etc/systemd/system
+export SYSTEMD_SYSTEM_DIR := /etc/systemd/system
+export SYSTEM_CERTS_FILE  := /usr/share/ca-certificates/extra/certs.pem
